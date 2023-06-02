e6606cc195b182748ce32023e7301bc7b0579f21
Anitha Subramanian <87901316+anithasubramanian19@users.noreply.github.com>
2023-06-02 13:47:02 +0530

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const imageOnes = document.querySelectorAll(".image-one")
+
+
+function leftArrow(){
+
+	imageOnes.forEach((imageOne)=>{
+		imageOne.classList.add("rotate")
+		
+	})
+}
+function rightArrow(){
+
+	imageOnes.forEach((imageOne)=>{
+		imageOne.classList.add("reverse")
+		
+	})
+}
